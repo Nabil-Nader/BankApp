2b295b39fdd9e01d0d4b1f9535471fc9ee80bc06
nabil <nabil.nader00@gmail.com>
2022-08-05 00:05:48 +0200

    fix missing requirement

--- src/main/java/com/bank/app/contoller/TransactionController.java
+++ src/main/java/com/bank/app/contoller/TransactionController.java
@@ -4,8 +4,6 @@ package com.bank.app.contoller;
 import com.bank.app.domain.Transaction;
 import com.bank.app.service.TransactionService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/bank/app/service/TransactionService.java
+++ src/main/java/com/bank/app/service/TransactionService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
